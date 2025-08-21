1e90eb2c1d1e1c16e939bdf6c293918287be101a
Agustina <agustina.sbergamo@globant.com>
2025-08-21 14:20:29 -0300

    Extract game setup values into constants

--- include/minefield/setup_game.h
+++ include/minefield/setup_game.h
@@ -1,6 +1,12 @@
 #pragma once
 #include "minefield.h"
 
+static const int MIN_BOARD_SIZE = 24; 
+static const int MAX_BOARD_SIZE = 50;
+static const int MIN_MINES = 3;
+static const int MAX_MINES = 8;
+static const int MAX_PLAYERS = 5;    
+
 NextState setupGame(GameContext& context);
 int readIntInRange(unsigned int min,
     unsigned int max,
