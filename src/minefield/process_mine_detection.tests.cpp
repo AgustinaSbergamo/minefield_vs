a47a43e52bb0a198acaf5cee68dd370895e239fc
Santiago Cristobal <santiago.cristobal@globant.com>
2025-08-27 17:02:46 -0300

    third batch of unit tests

--- src/minefield/process_mine_detection.tests.cpp
+++ src/minefield/process_mine_detection.tests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <minefield/process_mine_detection.h>
 #include <minefield/testing_utils.h>
+#include <minefield/select_mines.h>
 
 
 TEST(CELL, disableCell_disables_cell) {
@@ -21,4 +22,53 @@ TEST(CELL, disableCell_disables_cell) {
 		ASSERT_EQ(std::find(context.board.availableCells.begin(), context.board.availableCells.end(), cellToDisable), context.board.availableCells.end());
 	}
 }
+
+TEST(PLAYER, resizePlayerMineCount_shrinks_size) {
+}
+
+std::string setTestSelection(int i, unsigned int playersMineSize) {
+	int offset = i * playersMineSize;
+	std::string testInput;
+	for (int cell = 0; cell < playersMineSize; cell++) {
+		concatInput(testInput, cell + 1 + offset);
+	}
+	return testInput;
+}
+
+TEST(MINES, getExplodedMines_detects_self_hits) {
+	TestContext testContext;
+	GameContext &context = testContext.context;
+	setupTestGame(testContext, 3, 0);
+
+	std::string testInput;
+	unsigned int playerCount = context.players.size();
+	unsigned int playersMineSize = context.board.initialMines;
+
+	for (int i = 0; i < playerCount; i++) {
+		testInput.append(setTestSelection(i, playersMineSize));
+	}
+	setInputBuffer(testContext, testInput);
+	selectMines(context);
+
+	for (int i = 0; i < playerCount; i++) {
+		testInput.append(setTestSelection(i, playersMineSize));
+	}
+	setInputBuffer(testContext, testInput);
+	selectGuesses(context);
+
+	std::set<unsigned int> selfMineHits;
+	for (Player &player: context.players) {
+		std::set<unsigned int> playerSelfMineHits = getExplodedMines(player.mines, player.guesses);
+		selfMineHits.insert(playerSelfMineHits.begin(), playerSelfMineHits.end());
+	}
+
+	EXPECT_EQ(selfMineHits.size(), playersMineSize * playerCount);
+	
+}
+
+
+TEST(MINE_DETECTION, disableCell_disables_cell) {
+}
+
+
 
